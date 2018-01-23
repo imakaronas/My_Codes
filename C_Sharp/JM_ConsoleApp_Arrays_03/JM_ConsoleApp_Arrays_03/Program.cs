ad558121e96e51aeb89987988b2b3b1a3ef242d9
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:42:49 +0200

    Add files via upload
    C# Array Examples #03

---
+++ C_Sharp/JM_ConsoleApp_Arrays_03/JM_ConsoleApp_Arrays_03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_Arrays_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+            //You can use Enumerable.Range to create a range of numbers:
+            int[] arr = Enumerable.Range(1, 100).ToArray();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(arr[i]);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
