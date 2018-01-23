567e03c4dc4144f726d6ef335ce27d4f3a78053a
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:41:43 +0200

    Add files via upload
    C# Array Examples #01

---
+++ C_Sharp/JM_ConsoleApp_Arrays_01/JM_ConsoleApp_Arrays_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_Arrays_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myArray1 = new int[10];
+
+            for (int cnt = 0; cnt < 10; cnt++)
+            {
+                myArray1[cnt] = cnt;
+            }
+
+            for (int i = 0; i < myArray1.Length; i++)
+            {
+                Console.WriteLine(myArray1[i].ToString());
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
