b9fe87f4ebdbfb0147b7b6f5008310a2d533f5f7
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:55:55 +0200

    Add files via upload
    C# Split Array Tables Example #11

---
+++ C_Sharp/JM_ConsoleApp_SplitTables_11/JM_ConsoleApp_SplitTables_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_SplitTables_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //================================================
+            //How to Create an Array with different data types
+
+            object[] mixedArray = new object[4];
+            mixedArray[0] = 10;
+            mixedArray[1] = "Jack";
+            mixedArray[2] = true;
+            mixedArray[3] = System.DateTime.Now;
+
+            //In order to retrieve different data types from an Object array, you can convert an element to the appropriate data type
+            int id = int.Parse(mixedArray[0].ToString());
+            DateTime admissionDate = DateTime.Parse(mixedArray[3].ToString());
+
+
+            Console.WriteLine(admissionDate);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
