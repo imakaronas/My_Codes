60edd66f588780c0df753ce9a48c6a92091a17ec
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:52:27 +0200

    Add files via upload
    C# Split Array Tables Example #05

---
+++ C_Sharp/JM_ConsoleApp_SplitTables_05/JM_ConsoleApp_SplitTables_05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_SplitTables_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //================================================
+            //How can I test if an array contains a certain value?
+
+            string[] week2 = new string[7];
+            week2[0] = "Sunday";
+            week2[1] = "Monday";
+            week2[2] = "Tuesday";
+            week2[3] = "Wednsday";
+            week2[4] = "Thursday";
+            week2[5] = "friday";
+            week2[6] = "Saturday";
+
+            string value = "Wednsday";
+
+            int pos = Array.IndexOf(week2, value);
+
+            if (pos > -1)
+                Console.WriteLine(value + " exist !");
+            else
+                Console.WriteLine(value + " not exist !");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
