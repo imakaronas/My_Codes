2b6d883300afc260afed19b87f4f612da8cf98b2
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:51:53 +0200

    Add files via upload
    C# Split Array Tables Example #04

---
+++ C_Sharp/JM_ConsoleApp_SplitTables_04/JM_ConsoleApp_SplitTables_04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_SplitTables_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //================================================
+            //Converting String array to List
+
+            string[] week = new string[7];
+            week[0] = "Sunday";
+            week[1] = "Monday";
+            week[2] = "Tuesday";
+            week[3] = "Wednsday";
+            week[4] = "Thursday";
+            week[5] = "friday";
+            week[6] = "Saturday";
+
+            List<string> lst = new List<string>(week);
+
+            foreach (string day in lst)
+            {
+                Console.WriteLine(day);
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
