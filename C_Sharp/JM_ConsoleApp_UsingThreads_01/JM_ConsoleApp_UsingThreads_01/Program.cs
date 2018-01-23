1bb29091b678f9fe08f6904a3bc598c217ac344f
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:47:15 +0200

    Add files via upload
    C# Using Threads Example #01

---
+++ C_Sharp/JM_ConsoleApp_UsingThreads_01/JM_ConsoleApp_UsingThreads_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_UsingThreads_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread tid1 = new Thread(new ThreadStart(Thread1));
+            Thread tid2 = new Thread(new ThreadStart(Thread2));
+
+            tid1.Start();
+            tid2.Start();
+
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+
+        }
+
+        public static void Thread1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(string.Format("Thread1 {0}", i));
+                Thread.Yield();
+            }
+        }
+
+        public static void Thread2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(string.Format("Thread2 {0}", i));
+                Thread.Yield();
+            }
+        }
+
+    }
+}
