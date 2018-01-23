d306ceebe6f6df50b3a6e2c10944e40bd7496e89
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:02:53 +0200

    Add files via upload
    First Commit for Hello Programm

---
+++ C_Sharp/JM_ConsoleApp_Hello_Program/JM_ConsoleApp_Hello_Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_Hello_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+
+            Console.ReadLine();
+
+        }
+    }
+}
