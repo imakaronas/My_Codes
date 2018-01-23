59043753e1df285654b338663290acf4c7b4a950
Ioannis Makaronas <32853935+imakaronas@users.noreply.github.com>
2018-01-23 11:53:03 +0200

    Add files via upload
    C# Split Array Tables Example #06

---
+++ C_Sharp/JM_ConsoleApp_SplitTables_06/JM_ConsoleApp_SplitTables_06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_ConsoleApp_SplitTables_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //================================================
+            //How to sort an Integer Array
+
+
+            int[] myarray2 = new int[] { 3, 1, 4, 5, 2 };
+            Array.Sort(myarray2);
+            foreach (var str in myarray2)
+            {
+                Console.Write(str.ToString());
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
